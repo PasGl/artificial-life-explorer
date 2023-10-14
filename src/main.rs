3eb2e1bc3628e368a44c99001382d93e12f721c9
PasGl <pascalgleske+github@gmail.com>
2023-10-14 21:09:28 +0200

    Disable VSync

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,17 @@ mod cellular_automata;
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Artificial Life Explorer".into(),
+                    present_mode: bevy::window::PresentMode::AutoNoVsync,
+                    fit_canvas_to_parent: true,
+                    prevent_default_event_handling: false,
+                    window_theme: Some(bevy::window::WindowTheme::Dark),
+                    ..default()
+                }),
+                ..default()
+            }),
             EguiPlugin,
             //bevy::diagnostic::LogDiagnosticsPlugin::default(),
             //bevy::diagnostic::FrameTimeDiagnosticsPlugin,
@@ -17,8 +27,8 @@ fn main() {
             Update,
             (
                 cellular_automata::state::next_iteration,
-                cellular_automata::height_map::update_heightmap,
                 cellular_automata::egui_system,
+                cellular_automata::height_map::update_heightmap,
             ),
         )
         .run();
