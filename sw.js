1705eb656ed3d6baca13e664c573ea73d4f6fd0e
PasGl <pascalgleske+github@gmail.com>
2024-03-15 10:48:20 +0000

    Deploying to gh-pages from @ PasGl/artificial-life-explorer@4aa63c92d2423c7fad89d1446ab46ff5dbb1882e 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'artificial-life-explorer-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './artificial-life-explorer.js',
+  './artificial-life-explorer_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
